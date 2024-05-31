9d544ddba7039b40fa8bb15c4d568ae0c7deb0e6
William Jimenez <76670411+evdclick@users.noreply.github.com>
2024-05-30 21:33:14 -0500

    Update tablaConRandom.py
    Se completa funcionalidad del código de acuerdo a los criterios solicitados en el rango de celdas que requieren la respectiva aplicación

--- pdfFiller/tablaConRandom.py
+++ pdfFiller/tablaConRandom.py
@@ -1,29 +1,60 @@
 #Como primer paso debemos tratar de leer el contenido del archivo PDF mediante la librería PyPDF2
 #https://pypi.org/project/PyPDF2/
 from PyPDF2 import PdfReader, PdfWriter
+import random #Es para poder generar números aleatorios en los criterios especificados
 
 reader = PdfReader("base.pdf")
 writer = PdfWriter()
 numberOfPages = len(reader.pages)
-page = reader.pages[0]     #Leer pagina 0 porque es la única que tenemos
-writer.add_page(page)      #Preparar una página en blanco para un nuevo archivo
+page = reader.pages[0]     #Leer pagina 0
+writer.add_page(page)
 textContent = page.extract_text() #Extraer el contenido de la página cero
 
-print(textContent) #Ya en este punto hay una base para explorar el contenido y rellenar los campos segun
+#print(textContent) #Ya en este punto hay una base para explorar el contenido y rellenar los campos segun
                    #los criterios que solicitas
 
+
+
+                  
 celdasParaLlenar = reader.get_form_text_fields() #Las celdas para llenar son registros disponibles
                                                  #Y los voy a meter en un diccionario para fácil
                                                  #procesamiento
+"""Al imprimir el diccionario de las celdasParaLlenar
+obtengo una serie de items que se resume en las siguientes listas para
+aplicar los criterios que mencionas de evaluar su contenido y aplicar
+el número aleatorio que dices para cada caso:
+listKeyCoords1 = ["T",[9,64]] Aplicar valores entre 0.04 y 0.1
+listKeyCoords2 = ["E", [1, 52] Aplicar valores entre 0.07 y 0.18
+listKeyCoords3 = ["EE", [1, 7] Aplicar valores entre 0.07 y 0.18
+"""
+#Se procede entonces a alimentar tres listas que permitan identificar ese conjunto de celdas
+#Para así evaluarlas y poder aplicar los criterios de llenar con número aleatorio
+bex1 = []
+bex2 = []
+bex3 = []
+for i in range(9,64):
+ bex1.append('T'+str(i))
+for i in range(1,52):
+ bex2.append('E'+str(i))
+for i in range(1,7):
+ bex3.append('EE'+str(i))
+
 #https://pypdf2.readthedocs.io/en/3.x/user/forms.html
 #En este punto voy a enumerar todas las celdas disponibles para poder cargarle los números aleatorios
 #que recomiendas usar según la solicitud.
-fieldEnumerator=0
+#fieldEnumerator=0
 for key, values in celdasParaLlenar.items():
- writer.update_page_form_field_values(
-    writer.pages[0], {key: fieldEnumerator})   #Procedo a llenar con números consecutivos iniciando desde CERO
- fieldEnumerator+=1
+ print (key,values)
+ if key in bex1 and values!=None: #Estar seguro que la celda contiene algún número
+  fol = round(random.uniform(0.04,0.1),2)
+  writer.update_page_form_field_values(writer.pages[0], {key: fol})   #Procedo a llenar con números aleatorios entre 0.04 y 0.1
+ elif key in bex2 and values!=None:
+  fol = round(random.uniform(0.07,0.18),2)
+  writer.update_page_form_field_values(writer.pages[0], {key: fol})   #Procedo a llenar con números aleatorios entre 0.07 y 0.18
+ elif key in bex3 and values!=None:
+  fol = round(random.uniform(0.07,0.18),2)
+  writer.update_page_form_field_values(writer.pages[0], {key: fol})   #Procedo a llenar con números aleatorios entre 0.07 y 0.18
 
 # write "output" to PyPDF2-output.pdf
-with open("conCamposEnumerados.pdf", "wb") as output_stream:
+with open("conRandom.pdf", "wb") as output_stream:
     writer.write(output_stream)
